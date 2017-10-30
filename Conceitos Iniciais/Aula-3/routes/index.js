9bea275355409da656a5755aeb0402f00bec480d
ItaloBruno <italobruno_207@hotmail.com>
2017-10-30 18:38:34 -0300

    Conclusão aula 3 :)

--- Conceitos Iniciais/Aula-3/routes/index.js
+++ Conceitos Iniciais/Aula-3/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) =>{
+  var t = 'Expressssss';
+  if(req.query.title)
+    t = req.query.title;
+  res.render('index', { title: t });
 });
 
 module.exports = router;
