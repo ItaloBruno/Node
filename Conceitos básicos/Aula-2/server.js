3658341f7fb9dccc87c88216b5ead1f900e68a9c
ItaloBruno <italobruno_207@hotmail.com>
2017-10-30 15:09:48 -0300

    Aula 2 concluída

--- Conceitos básicos/Aula-2/server.js
+++ Conceitos básicos/Aula-2/server.js
@@ -1,14 +1,31 @@
 /**
- * Criando um pequeno servidor na porta 3000
+ * Criando um pequeno servidor na porta 3000, onde o usário pode acessar os arquivos definidos no servidor.
+ * Se o usuário tentar acessar um arquivo inexistente, o servidor vai retornar uma página de erro.
  */
 
 var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer((request, response)=>{
-    response.writeHead(200, {'Content-type': 'text/html; charset = utf-8'});
-    response.write('<h1>Tulio Maravilha</h1>');
-    response.end();
+    var page = 'index.html';
+    if (request.url != '/'){
+        page = request.url + '.html';
+    }
+
+    fs.readFile('./public/' + page, (err, data)=>{
+        var headStatus = 200;
+
+        if(err){
+            headStatus = 404;
+            data = fs.readFileSync('./error/404.html');
+        }
+            
+        response.writeHead(headStatus, {'Content-type': 'text/html; charset = utf-8'});
+        response.write(data);
+        response.end();
+    });
+
+    
 });
 
 server.listen(3000, console.log('Escutando na porta 3000!'));
