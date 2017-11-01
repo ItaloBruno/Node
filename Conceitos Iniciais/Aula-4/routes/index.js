3717b95303f4019216886e38ac47694148000c57
ItaloBruno <italobruno_207@hotmail.com>
2017-11-01 12:24:02 -0300

    4/5 do CRUD com Express e Mongodb

--- Conceitos Iniciais/Aula-4/routes/index.js
+++ Conceitos Iniciais/Aula-4/routes/index.js
@@ -10,7 +10,28 @@ router.get('/', function(req, res, next) {
     
     res.render('index', { title: 'Express',tasks: tasks});
   });
-  
+});
+
+router.post('/add', (req, res, next)=>{
+  var body = req.body;
+  body.status = false;
+  model.create(body,(err, task) =>{
+    if(err)
+      throw err;
+    res.redirect('/');
+  });
+});
+
+router.get('/turn/:id', (req, res, next)=>{
+  var id = req.params.id;
+  model.findById(id, (err, task)=>{
+    if(err)
+      throw err;
+    task.status = !task.status;
+    task.save(()=>{
+      res.redirect('/');
+    });
+  });
 });
 
 module.exports = router;
