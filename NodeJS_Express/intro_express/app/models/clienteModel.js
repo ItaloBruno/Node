25ba13837f9c8cdbf1ff8289fc31bf8e98162010
ItaloBruno <italobruno_207@hotmail.com>
2017-11-22 10:40:10 -0300

    Conectando com o banco de dados

--- NodeJS_Express/intro_express/app/models/clienteModel.js
+++ NodeJS_Express/intro_express/app/models/clienteModel.js
@@ -1,6 +1,22 @@
+const mysql = require('mysql');
+
+const conexao = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password:'1234',
+    database: 'curso_node'
+});
+
+
+
 //Regras de negócio
 module.exports = () =>{
     this.all = () =>{
+
+        conexao.query('SELECT * FROM clientes', (erro, resultado) =>{
+            console.log(resultado);
+        });
+
         return [
             {nome: 'Italo', email: 'italobruno_207@hotmail.com'},
             {nome: 'Maria', email: 'mariadobairro@hotmail.com'}
