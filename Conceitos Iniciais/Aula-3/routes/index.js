80c689b923ac29d14b5d6a473a99f977a4203e5c
edilson14 <edytavares66@gmail.com>
2021-10-31 16:43:07 -0300

    formatação de textos

--- Conceitos Iniciais/Aula-3/routes/index.js
+++ Conceitos Iniciais/Aula-3/routes/index.js
@@ -1,12 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) =>{
-  var t = 'Expressssss';
-  if(req.query.title)
-    t = req.query.title;
-  res.render('index', { title: t });
+router.get("/", (req, res, next) => {
+  var t = "Expressssss";
+  if (req.query.title) t = req.query.title;
+  res.render("index", { title: t });
 });
 
 module.exports = router;
